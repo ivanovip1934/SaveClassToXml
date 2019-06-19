71c340e42697759c17548917e67027931db1a832
ivanovip1934 <ivanovip1934@outlook.com>
2019-06-19 22:54:57 +0300

    написал простой сериалайзер класса.

--- SaveClassToXml/Program.cs
+++ SaveClassToXml/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
 
 namespace SaveClassToXml
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            TestClass tc = new TestClass();
+            tc.PathToMsiX64 = @"\\fileserv.omsu.vmr\soft_distribution$\7-zip\1900\7z-x64.msi";
+            tc.PathToMsiX86 = @"\\fileserv.omsu.vmr\soft_distribution$\7-zip\1900\7z.msi";
+            tc.Property = "";
+            tc.ForceRestart = false;
+
+
+
+                string pathToXml = @"C:\programs\!!!!\conf.xml";
+                XmlSerializer serializer = new XmlSerializer(typeof(TestClass));
+            TextWriter writer = new StreamWriter(pathToXml);
+            serializer.Serialize(writer, tc);
+            writer.Close();
+
+
+
         }
     }
 }
---
+++ SaveClassToXml/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace SaveClassToXml
+{
+    [XmlRoot("TestClass",IsNullable =false)]
+    [Serializable]
+    public class TestClass
+    {
+        public string PathToMsiX86 { get; set; }
+        public string PathToMsiX64 { get; set; }
+        public string Property { get; set; }
+        public bool ForceRestart { get; set; }
+        
+    }
+}
